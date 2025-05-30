22b592ea568d371947fd15b1b418424a559e53f8
MicahOh <moh0001@mymail.lausd.net>
2025-05-30 10:34:58 -0700

    Update test.java

--- src/main/java/com/kenTakakura/test.java
+++ src/main/java/com/kenTakakura/test.java
@@ -10,6 +10,14 @@ public class test {
         ado.setTurn("L", 720, 100);
         ado.setDisplay(arr);
         ado.setMove("F", 50, 30);
+
+        int[] aa = {0, 0, 0, 0, 0, 
+                    0, 1, 0, 0, 0, 
+                    1, 1, 1, 0, 1, 
+                    0, 1, 0, 1, 0, 
+                    0, 1, 1, 0, 0};
+        ado.setDisplay(aa);
+        ado.pause(2.0);
        
         ado.stopAll();
         ado.disconnect();
