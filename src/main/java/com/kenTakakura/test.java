a09e5a056ad1cf50f7fe286d785bd1f26ca2cb1b
MicahOh <moh0001@mymail.lausd.net>
2025-05-30 10:24:15 -0700

    Update test.java

--- src/main/java/com/kenTakakura/test.java
+++ src/main/java/com/kenTakakura/test.java
@@ -6,7 +6,7 @@ public class test {
     public static void main(String[] args) {
         Finch ado = new Finch();
 
-        int[] arr = {1, 0, 1, 1, 1, 1, 0, 1,0,0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1}
+        int[] arr = {1, 0, 1, 1, 1, 1, 0, 1,0,0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1};
         ado.setTurn("L", 720, 100);
         ado.setDisplay(arr);
        
