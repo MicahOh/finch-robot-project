561cedff08b22677897da76be60ea171708b7131
MicahOh <moh0001@mymail.lausd.net>
2025-05-30 10:31:13 -0700

    Update test.java

--- src/main/java/com/kenTakakura/test.java
+++ src/main/java/com/kenTakakura/test.java
@@ -5,11 +5,11 @@ import com.birdbrain.Finch;
 public class test {
     public static void main(String[] args) {
         Finch ado = new Finch();
-
+        // credit to Arman hayrapetyan
         int[] arr = {1, 0, 1, 1, 1, 1, 0, 1,0,0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1};
         ado.setTurn("L", 720, 100);
         ado.setDisplay(arr);
-        ado.setMove("F", 80, 60);
+        ado.setMove("F", 50, 30);
        
         ado.stopAll();
         ado.disconnect();
