e98f723a653b83310518cd95b3d839b4b3b17f6a
MicahOh <moh0001@mymail.lausd.net>
2025-05-30 10:23:50 -0700

    Update test.java

--- src/main/java/com/kenTakakura/test.java
+++ src/main/java/com/kenTakakura/test.java
@@ -6,8 +6,9 @@ public class test {
     public static void main(String[] args) {
         Finch ado = new Finch();
 
+        int[] arr = {1, 0, 1, 1, 1, 1, 0, 1,0,0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1}
         ado.setTurn("L", 720, 100);
-
+        ado.setDisplay(arr);
        
         ado.stopAll();
         ado.disconnect();
