e3871aa19c2be597b7ed466ed105779478fdfb09
MicahOh <moh0001@mymail.lausd.net>
2025-05-30 10:16:18 -0700

    Update test.java

--- src/main/java/com/kenTakakura/test.java
+++ src/main/java/com/kenTakakura/test.java
@@ -1,4 +1,15 @@
-public String ado(){
-    String ztmy = "fjj";
-    return ztmy;
+package com.kenTakakura;
+
+import com.birdbrain.Finch;
+
+public class test {
+    public static void main(String[] args) {
+        Finch ado = new Finch();
+
+        myFinch.setTurn("L", 720. 100);
+
+       
+        myFinch.stopAll();
+        myFinch.disconnect();
+    }
 }
