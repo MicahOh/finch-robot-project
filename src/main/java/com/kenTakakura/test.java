7b377e0d88ed882280fc312c3d9664793262437e
MicahOh <moh0001@mymail.lausd.net>
2025-06-02 09:59:21 -0700

    Update test.java

--- src/main/java/com/kenTakakura/test.java
+++ src/main/java/com/kenTakakura/test.java
@@ -6,17 +6,21 @@ public class test {
     public static void main(String[] args) {
         Finch ado = new Finch();
         // credit to Arman hayrapetyan
-        int[] arr = {1, 0, 1, 1, 1, 1, 0, 1,0,0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1};
+        int[] arr = {0, 0, 0, 0, 0, 
+                     0, 1, 0, 1, 0, 
+                     0, 0, 0, 0, 0, 
+                     1, 0, 0, 0, 1, 
+                     0, 1, 1, 1, 0};
         ado.setTurn("L", 720, 100);
         ado.setDisplay(arr);
         
         ado.setMove("F", 50, 30);
 
-        int[] aa = {0, 1, 0, 0, 0, 
-                    1, 1, 1, 0, 1, 
-                    0, 1, 0, 1, 0, 
-                    0, 1, 1, 0, 0, 
-                    0, 1, 0, 0, 0};
+        int[] aa = {1, 1, 1, 1, 1, 
+                    1, 0, 1, 0, 1, 
+                    1, 1, 1, 1, 1, 
+                    0, 1, 1, 1, 0, 
+                    1, 0, 0, 0, 1};
         ado.setDisplay(aa);
         ado.pause(2.0);
        
