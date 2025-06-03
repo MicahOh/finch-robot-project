a2527bb6e6e239785283e17c0b9c73bd61b07a81
MicahOh <moh0001@mymail.lausd.net>
2025-06-03 10:00:22 -0700

    Update test.java

--- src/main/java/com/kenTakakura/test.java
+++ src/main/java/com/kenTakakura/test.java
@@ -6,32 +6,69 @@ public class test {
     public static void main(String[] args) {
         Finch ado = new Finch();
         // credit to Arman hayrapetyan
-        int[] arr = {0, 0, 0, 0, 0, 
-                     0, 1, 0, 1, 0, 
-                     0, 0, 0, 0, 0, 
-                     1, 0, 0, 0, 1, 
-                     0, 1, 1, 1, 0};
-        ado.setTurn("L", 720, 100);
-        ado.setDisplay(arr);
+        // int[] arr = {0, 0, 0, 0, 0, 
+        //              0, 1, 0, 1, 0, 
+        //              0, 0, 0, 0, 0, 
+        //              1, 0, 0, 0, 1, 
+        //              0, 1, 1, 1, 0};
+        // ado.setTurn("L", 720, 100);
+        // ado.setDisplay(arr);
         
-        ado.setMove("F", 50, 30);
+        // ado.setMove("F", 50, 30);
 
-        int[] aa = {1, 1, 1, 1, 1, 
-                    1, 0, 1, 0, 1, 
-                    1, 1, 1, 1, 1, 
-                    0, 1, 1, 1, 0, 
-                    1, 0, 0, 0, 1};
-        ado.setDisplay(aa);
-        ado.pause(2.0);
+        // int[] aa = {1, 1, 1, 1, 1, 
+        //             1, 0, 1, 0, 1, 
+        //             1, 1, 1, 1, 1, 
+        //             0, 1, 1, 1, 0, 
+        //             1, 0, 0, 0, 1};
+        // ado.setDisplay(aa);
+        // ado.pause(2.0);
+        
+        //licky
+        // ado.playNote(100, 0.5);
+        // ado.pause(0.2);
+        // ado.playNote(102, 0.5);
+        // ado.pause(0.2);
+        // ado.playNote(103, 0.5);
+        // ado.pause(0.2);
+        // ado.playNote(105, 0.5);
+        // ado.pause(0.2);
+        // ado.playNote(102, 1.0);
+        // ado.pause(0.4);
+        // ado.playNote(98, 0.5);
+        // ado.pause(0.2);
+        // ado.playNote(100, 0.5);
+        // ado.pause(0.2);
+        // ado.pause(2.0);
 
-        ado.playNote(60, 0.5);
-        ado.playNote(61, 0.5);
-        ado.playNote(62, 0.5);
-        ado.playNote(63, 0.5);
-        ado.playNote(61, 1);
-        ado.playNote(59, 0.5);
-        ado.playNote(60, 0.5);
-       
+
+        //asian
+        // ado.playNote(95, 0.075);
+        // ado.pause(0.1);
+        // ado.playNote(95, 0.075);
+        // ado.pause(0.1);
+        // ado.playNote(95, 0.075);
+        // ado.pause(0.1);
+        // ado.playNote(95, 0.075);
+        // ado.pause(0.1);
+        // ado.playNote(93, 0.075);
+        // ado.pause(0.2);
+        // ado.playNote(93, 0.075);
+        // ado.pause(0.2);
+        // ado.playNote(90, 0.075);
+        // ado.pause(0.2);
+        // ado.playNote(90, 0.075);
+        // ado.pause(0.2);
+        // ado.playNote(93, 0.5);
+        // ado.pause(0.4);
+
+
+        ado.setTurn("R", 720, 100);
+        ado.setTurn("L", 360, 100);
+        ado.setTurn("R", 451, 100);
+        ado.setTurn("L", 335, 100);
+
+
         ado.stopAll();
         ado.disconnect();
     }
