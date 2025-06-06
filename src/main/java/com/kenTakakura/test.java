42cbd5d915145b79b9bc7fdb2169d42efb00ffde
MicahOh <moh0001@mymail.lausd.net>
2025-06-06 08:31:50 -0700

    e
    e

--- src/main/java/com/kenTakakura/test.java
+++ src/main/java/com/kenTakakura/test.java
@@ -6,40 +6,76 @@ public class test {
     public static void main(String[] args) {
         Finch ado = new Finch();
         // credit to Arman hayrapetyan
-        // int[] arr = {0, 0, 0, 0, 0, 
-        //              0, 1, 0, 1, 0, 
-        //              0, 0, 0, 0, 0, 
-        //              1, 0, 0, 0, 1, 
-        //              0, 1, 1, 1, 0};
-        // ado.setTurn("L", 720, 100);
-        // ado.setDisplay(arr);
-        
-        // ado.setMove("F", 50, 30);
+        int[] arr = {0, 0, 0, 0, 0, 
+                     0, 0, 0, 0, 0, 
+                     1, 0, 0, 1, 0, 
+                     0, 0, 0, 0, 0, 
+                     0, 0, 0, 0, 0};
+        ado.setDisplay(arr); 
+        ado.pause(0.1);   
+        int[] A = {0, 0, 0, 0, 0, 
+               0, 0, 0, 0, 0, 
+               0, 1, 0, 1, 0, 
+               0, 0, 0, 0, 0, 
+               0, 0, 0, 0, 0};
+        ado.setDisplay(A); 
+        ado.pause(0.1); 
+        int[] AA = {0, 0, 0, 0, 0, 
+               0, 0, 0, 0, 0, 
+               0, 0, 1, 1, 0, 
+               0, 0, 0, 0, 0, 
+               0, 0, 0, 0, 0};
+        ado.setDisplay(AA); 
+        ado.pause(0.1); 
+        //fruit
+        int[] Arr = {0, 0, 0, 0, 0, 
+               0, 0, 0, 0, 0, 
+               0, 0, 1, 1, 0, 
+               1, 0, 0, 0, 0, 
+               0, 0, 0, 0, 0};
+        ado.setDisplay(Arr); 
+        ado.playNote(95, 0.075);
+        ado.pause(0.1);
 
-        // int[] aa = {1, 1, 1, 1, 1, 
-        //             1, 0, 1, 0, 1, 
-        //             1, 1, 1, 1, 1, 
-        //             0, 1, 1, 1, 0, 
-        //             1, 0, 0, 0, 1};
-        // ado.setDisplay(aa);
-        // ado.pause(2.0);
+        int[] Arr = {0, 0, 0, 0, 0, 
+                    0, 0, 0, 0, 0, 
+                    0, 0, 0, 1, 0, 
+                    1, 0, 0, 1, 0, 
+                    0, 0, 0, 0, 0};
+
+        int[] Arr = {0, 0, 0, 0, 0, 
+                    0, 0, 0, 0, 0, 
+                    0, 0, 0, 0, 0, 
+                    1, 0, 1, 1, 0, 
+                    0, 0, 0, 0, 0};
+
+        int[] Arr = {0, 0, 0, 0, 0, 
+                    0, 0, 0, 0, 0, 
+                    0, 0, 0, 0, 0, 
+                    1, 1, 1, 1, 0, 
+                    0, 0, 0, 0, 0};
         
-        //licky
-        // ado.playNote(100, 0.5);
-        // ado.pause(0.2);
-        // ado.playNote(102, 0.5);
-        // ado.pause(0.2);
-        // ado.playNote(103, 0.5);
-        // ado.pause(0.2);
-        // ado.playNote(105, 0.5);
-        // ado.pause(0.2);
-        // ado.playNote(102, 1.0);
-        // ado.pause(0.4);
-        // ado.playNote(98, 0.5);
-        // ado.pause(0.2);
-        // ado.playNote(100, 0.5);
-        // ado.pause(0.2);
-        // ado.pause(2.0);
+
+        //fruit
+
+        int[] Arr = {0, 0, 0, 0, 0, 
+                     0, 0, 0, 0, 0, 
+                     0, 0, 0, 1, 0, 
+                      1, 0, 0, 1, 0, 
+                       0, 0, 0, 0, 0};
+        
+        
+
+
+
+
+
+
+
+
+
+
+
 
 
         //asian
@@ -63,10 +99,9 @@ public class test {
         // ado.pause(0.4);
 
 
-        ado.setTurn("R", 720, 100);
-        ado.setTurn("L", 360, 100);
-        ado.setTurn("R", 451, 100);
-        ado.setTurn("L", 335, 100);
+
+
+
 
 
         ado.stopAll();
